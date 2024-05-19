3eaf52ef17883bbb22090bce1b8fd1aabc530831
shaiza <shaiza@uoguelph.ca>
2024-05-18 21:37:58 -0400

    cleaned up stylesheet linkage

--- vitrum/src/components/navigation.js
+++ vitrum/src/components/navigation.js
@@ -4,34 +4,42 @@ import { useEffect, useState, useContext } from 'react';
 
 import { NearContext } from '@/context';
 import Logo from '/public/logo.svg';
+import Help from '../assets/help.svg';
+import Accessible from '../assets/accessible.svg';
+
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {faCircleQuestion } from "@fortawesome/fa-solid";
 
 export const Navigation = () => {
-  const { signedAccountId, wallet } = useContext(NearContext);
-  const [action, setAction] = useState(() => { });
-  const [label, setLabel] = useState('Loading...');
+//   const { signedAccountId, wallet } = useContext(NearContext);
+//   const [action, setAction] = useState(() => { });
+//   const [label, setLabel] = useState('Loading...');
 
-  useEffect(() => {
-    if (!wallet) return;
+//   useEffect(() => {
+//     if (!wallet) return;
 
-    if (signedAccountId) {
-      setAction(() => wallet.signOut);
-      setLabel(`Logout ${signedAccountId}`);
-    } else {
-      setAction(() => wallet.signIn);
-      setLabel('Login');
-    }
-  }, [signedAccountId, wallet]);
+//     if (signedAccountId) {
+//       setAction(() => wallet.signOut);
+//       setLabel(`Logout ${signedAccountId}`);
+//     } else {
+//       setAction(() => wallet.signIn);
+//       setLabel('Login');
+//     }
+//   }, [signedAccountId, wallet]);
 
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <Link href="/" passHref legacyBehavior>
-          <Image priority src={Logo} alt="NEAR" width="30" height="24" className="d-inline-block align-text-top" />
+    <div className='nav'>
+      <Link href="/" passHref legacyBehavior>
+        <Image src={Logo} alt="VITRUM" className="vitrum-logo" />
+      </Link>
+      <div>
+        <Link href="/help" passHref legacyBehavior>
+          <Image src={Help} alt="HELP" className="icons-nav" />
         </Link>
-        <div className='navbar-nav pt-1'>
-          <button className="btn btn-secondary" onClick={action} > {label} </button>
-        </div>
+        <Link href="/help" passHref legacyBehavior>
+          <Image src={Accessible} alt="HELP" className="icons-nav" />
+        </Link>
       </div>
-    </nav>
+    </div>
   );
 };
